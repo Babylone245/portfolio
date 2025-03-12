94157347b89db95ea9645b387ca334c65e9d83e1
Steven RUBAT DU MERAC <s.rubatdumerac@groupe-aen.info>
2025-03-12 18:54:28 +0100

    First commit portfolio

---
+++ src/app/accueil/accueil-component/accueil-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil-component',
+  templateUrl: './accueil-component.component.html',
+  styleUrls: ['./accueil-component.component.scss']
+})
+export class AccueilComponentComponent {
+
+}
